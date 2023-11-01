a1e92d0ccd22fded89f1046048058caab8ac1e1b
Ben_Langlois <benlangloiss@gmail.com>
2023-11-01 15:57:29 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div id='header'>
+        <h1>Ben Langlois</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
