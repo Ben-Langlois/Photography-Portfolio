17882551ff8f15eeee1429fc9d777d8d5f2f2fff
Ben_Langlois <benlangloiss@gmail.com>
2023-11-02 17:35:21 -0400

    added/imported images, made grid to display, just working on state to hold them

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import './App.scss';
 
-// Images
+/* #region Images */
 import github from './socials/iconmonstr-github-3.svg';
 import linkedin from './socials/iconmonstr-linkedin-3.svg';
+
+// Find a way to do this in fewer lines of code
+// Concept from https://www.shecodes.io/athena?tag=require.context#questions 
+const Addington = require.context('./images/Addington', true);
+const AddingtonArr = Addington.keys().map(image => Addington(image));
+// console.log(AddingtonArr);
+// Costarica
+const Costarica = require.context('./images/Costarica', true);
+const CostaricaArr = Costarica.keys().map(image => Costarica(image));
+// Georgianbay
+const Georgianbay = require.context('./images/Georgianbay', true);
+const GeorgianbayArr = Georgianbay.keys().map(image => Georgianbay(image));
+// Scotland
+const Scotland = require.context('./images/Scotland', true);
+const ScotlandArr = Scotland.keys().map(image => Scotland(image));
+
+//https://www.samdawson.dev/article/auto-flow-dense-varying-image-sizes
+// function ImageGridItem({ image }) {
+//   const style = {
+//     gridColumnEnd: `span ${getSpanEstimate(image.width)}`,
+//     gridRowEnd: `span ${getSpanEstimate(image.height)}`,
+//   }
+//   return <img style={style} src={image.url} alt={image.alt} />
+// }
+// function getSpanEstimate(size) {
+//   if (size > 250) {
+//     return 2
+//   }
+//   return 1
+// }
+
 
 function App() {
   return (
@@ -17,13 +48,17 @@ function App() {
           <h3>Costa Rica</h3>
         </div>
         <div id='socials'>
-          <a href='https://github.com/Ben-Langlois/Photography-Portfolio'><img id='github' src={github}/></a>
-          <a href='https://www.linkedin.com/in/benjaminlanglois/'><img id='linkedin' src={linkedin}/></a>
+          <a href='https://github.com/Ben-Langlois/Photography-Portfolio'><img id='github' alt='github' src={github}/></a>
+          <a href='https://www.linkedin.com/in/benjaminlanglois/'><img id='linkedin' alt='linkedin' src={linkedin}/></a>
           <h4>Nov 2023</h4>
         </div>
       </div>
       <div id='dashboard'>
-        
+      {
+        AddingtonArr.map((image, index) => (
+          <img key={index} src={image} alt='...'/> 
+        ))
+      }
       </div>
     </div>
   );
