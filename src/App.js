e0378c70c8cd867ec2d34a7f79533408fa38396e
Ben_Langlois <benlangloiss@gmail.com>
2023-11-01 21:51:51 -0400

    made container, made nav, just working on hover anim then moving onto importing photos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.scss';
+
+// Images
+import github from './socials/iconmonstr-github-3.svg';
+import linkedin from './socials/iconmonstr-linkedin-3.svg';
 
 function App() {
   return (
-    <div className="App">
-      <div id='header'>
-        <h1>Ben Langlois</h1>
+    <div className="container">
+      <div id='nav'>
+        <h1>Ben Langlois.</h1>
+        <h4>Photography</h4>
+        <div id='links'>
+          <h3>GTA</h3>
+          <h3>Georgian Bay</h3>
+          <h3>Addington Highlands</h3>
+          <h3>Costa Rica</h3>
+        </div>
+        <div id='socials'>
+          <a href='https://github.com/Ben-Langlois/Photography-Portfolio'><img id='github' src={github}/></a>
+          <a href='https://www.linkedin.com/in/benjaminlanglois/'><img id='linkedin' src={linkedin}/></a>
+          <h4>Nov 2023</h4>
+        </div>
+      </div>
+      <div id='dashboard'>
+        
       </div>
     </div>
   );
