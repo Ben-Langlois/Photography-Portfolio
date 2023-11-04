6061c782dd33eadc41b1a0a5b9d7125588a6b166
Ben_Langlois <benlangloiss@gmail.com>
2023-11-03 22:42:25 -0400

    mobile queries presentable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import './App.scss';
-
 /* #region Images */
 import github from './socials/iconmonstr-github-3.svg';
 import linkedin from './socials/iconmonstr-linkedin-3.svg';
@@ -22,14 +21,30 @@ const ScotlandArr = Scotland.keys().map(image => Scotland(image));
 
 const fullAlbum = [...AddingtonArr, ...CostaricaArr, ...GeorgianbayArr, ...ScotlandArr];
 
+
 function App() {
   const [images, setImages] = useState([...fullAlbum]); 
+  const toggleMenu = () => {
+    document.getElementById("menu-container").classList.toggle("change");
+    document.getElementById("dropdown-menu").classList.toggle("show");    
+  }
 
   return (
     <div className="container">
       <div id='nav'>
         <h1 onClick={() => setImages([...fullAlbum])}>Ben Langlois.</h1>
         <h4>Photography</h4>
+          <div id="menu-container" class="menu-container" onClick={toggleMenu}>
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+            <div id='dropdown-menu'class="dropdown-content" onClick={toggleMenu}> {/* why must it be on the parent AND the children */}
+              <h3 class='link' onClick={() => setImages([...ScotlandArr])}>Scotland</h3>
+              <h3 class='link' onClick={() => setImages([...GeorgianbayArr])}>Georgian Bay</h3>
+              <h3 class='link' onClick={() => setImages([...AddingtonArr])}>Addington Highlands</h3>
+              <h3 class='link' onClick={() => setImages([...CostaricaArr])}>Costa Rica</h3>
+            </div>
+        </div>
         <div id='links'>
           <h3 class='link' onClick={() => setImages([...ScotlandArr])}>Scotland</h3>
           <h3 class='link' onClick={() => setImages([...GeorgianbayArr])}>Georgian Bay</h3>
